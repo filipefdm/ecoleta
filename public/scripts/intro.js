8b76a75be09e0f1f22bae3ea3e6f571a289f4276
Filipe Motta <femotta96@gmail.com>
2020-10-08 17:10:35 -0300

    initial commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,65 @@
+// Comments:
+// document.write("Hello")
+// variáveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também` 
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     document.write("Executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// Registrar função:
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // Executar:
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// --- Condicionais ---
+
+// const notaFinal = 4
+
+// if (notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado!")
+// }
+
+// Loop repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
